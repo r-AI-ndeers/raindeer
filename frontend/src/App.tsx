9b9c0be87d76814a8a7b1219a4597c9be66ad92a
MaksymPetyak <petyak.mi@gmail.com>
2022-12-09 20:21:38 +0100

    Adding material UI and small starting page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import mainbackground from './assets/mainbackground.jpg';
+import {Box, Button, CssBaseline, Typography} from "@mui/material";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CssBaseline />
+      <Box
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        style={{
+            backgroundImage: `url(${mainbackground})`,
+            backgroundSize: "cover",
+        }}
+      >
+          <Box maxWidth={"800px"} display={"flex"} flexDirection={"column"} alignItems={"center"} gap={"64px"}>
+              <Typography variant="h1">
+                  rAIndeer
+              </Typography>
+              <Typography variant="h3">
+                  Create personalised cards for people dear to you with AI!
+              </Typography>
+              <Box>
+                  <Button variant={"contained"} style={{backgroundColor: "#2E7D32"}}>Create a card</Button>
+              </Box>
+          </Box>
+      </Box>
     </div>
   );
 }
