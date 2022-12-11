ddc9ca83070b890acc1adc4ac754db60200d8edb
MaksymPetyak <petyak.mi@gmail.com>
2022-12-11 14:08:40 +0100

    Fix consts

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -1,6 +1,4 @@
-
-
 // FIXME: do this through env variable
-export const BACKEND_URL = "http://localhost:8000";
+export const BACKEND_URL = "https://raindeers-backend.herokuapp.com";
 export const FRONTEND_URL = "https://raindeers.netlify.app"
-export const MOCK_CARD_ID = "123";
+export const MOCK_CARD_ID = "5978683c-2e32-439f-8658-8131d3b1c658";
