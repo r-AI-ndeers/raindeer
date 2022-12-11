2e33f4733d1bf7e434d87896fab7926aa529af09
MaksymPetyak <petyak.mi@gmail.com>
2022-12-11 19:14:31 +0100

    Make the home page more like landing page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Box, Button, Typography} from "@mui/material";
 import {ImageBackgroundLayout} from "../components/Layout";
 import {MOCK_CARD_ID} from "../consts";
+import ImageIcon from '@mui/icons-material/Image';
+import EditIcon from '@mui/icons-material/Edit';
 
 
 export function Home() {
@@ -10,7 +12,7 @@ export function Home() {
             <Box maxWidth={"800px"} display={"flex"} flexDirection={"column"}
                  alignItems={"center"} gap={"64px"}>
                 <Typography variant="h1">
-                    rAIndeer
+                    <b>rAIndeer</b>
                 </Typography>
                 <Typography variant="h3">
                     Create personalised cards for people dear to you with AI!
@@ -33,6 +35,30 @@ export function Home() {
                             Create a card
                         </Typography>
                     </Button>
+                </Box>
+                <Box display={"flex"} flexDirection={"row"} alignItems={"center"} gap={"64px"}>
+                    <Box display={"flex"} flexDirection={"column"} gap={"16px"} alignItems={"center"} justifyContent={"center"}>
+                        <Box display="flex" alignItems={"center"} gap={"16px"}>
+                            <ImageIcon style={{fontSize:"48px"} }/>
+                            <Typography variant={"h4"}>
+                                Photos
+                            </Typography>
+                        </Box>
+                        <Typography variant={"h5"} textAlign={"center"}>
+                            Upload photo of you or your friends and have them modified for the Christmas spirit!
+                        </Typography>
+                    </Box>
+                    <Box display={"flex"} flexDirection={"column"} gap={"16px"} alignItems={"center"} textAlign={"center"}>
+                        <Box display="flex" alignItems={"center"} gap={"16px"}>
+                            <EditIcon style={{fontSize:"48px"} }/>
+                            <Typography variant={"h4"}>
+                                Poem
+                            </Typography>
+                        </Box>
+                        <Typography variant={"h5"}>
+                            Our AI will generate a unique poem for you based on some facts about the person.
+                        </Typography>
+                    </Box>
                 </Box>
             </Box>
         </ImageBackgroundLayout>
