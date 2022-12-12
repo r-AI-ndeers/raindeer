44772bc076cb2b45f395640a2d4835f3f6cb334c
MaksymPetyak <petyak.mi@gmail.com>
2022-12-12 18:45:52 +0100

    Add some styling to the final image

--- frontend/src/pages/ViewCard.tsx
+++ frontend/src/pages/ViewCard.tsx
@@ -54,6 +54,9 @@ export function ViewCard() {
                             maxWidth: "100%",
                             display: "block",
                             margin: "auto",
+                            boxShadow: "0.3px 0.5px 0.7px hsl(286deg 36% 56% / 36%), 0.8px 1.6px 2px -0.8px hsl(286deg 36% 56% / 36%), 2.1px 4.1px 5.2px -1.7px hsl(286deg 36% 56% / 36%), 5px 10px 12.6px -2.5px hsl(286deg 36% 56% / 36%)",
+                            borderRadius: "16px",
+                            transform: "rotate(-2deg)",
                         }}
                         src={viewData.image}
                     />
