c9a681b9852c0ebc09bff4ab1fad7bec160e9c78
MaksymPetyak <petyak.mi@gmail.com>
2022-12-18 02:21:32 +0100

    Add Dockerfile and redeploy

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -1,4 +1,3 @@
-// FIXME: do this through env variable
-export const BACKEND_URL = "https://raindeers-backend.herokuapp.com";
+export const BACKEND_URL = "https://raindeers-cu4575bk2a-ew.a.run.app";
 export const FRONTEND_URL = "https://raindeers.netlify.app"
 export const MOCK_CARD_ID = "e667c8a3-c5a0-4cf7-80ea-d8883b5390e8";
